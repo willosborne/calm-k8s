315afea1219443d6623a67d4bcfd9bf551056778
Will Osborne <will.osborne@morganstanley.com>
2024-08-08 15:11:34 +0100

    Basic template

--- src/commands/generate/generate.ts
+++ src/commands/generate/generate.ts
@@ -1,4 +1,21 @@
+import Handlebars from "handlebars";
+import { promises as fs } from 'node:fs';
+
+async function serviceTemplate() {
+    const templateFile = await fs.readFile("templates/service.template.yaml", { encoding: 'utf-8' });
+    console.log(templateFile)
+    return Handlebars.compile(templateFile);
+}
+
 export default async function(filename: string) {
     console.log("generating from " + filename);
-    return "done"
+    const template = await serviceTemplate();
+    const out = template({
+        'port': 1234,
+        'targetPort': 12345,
+        'name': "egg",
+        'label': 'label'
+    })
+    console.log(out)
+    return out;
 }
